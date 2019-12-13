ef149a6886c9b7936a71317ddd0c74170f6eb52f
Miri <mirimarkovitzbl@gmail.com>
2019-12-12 18:28:45 -0800

    add ListView component and integrating the washroom list from the DB into the code.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
 import Washroom from './washroomInfo.jsx';
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
     <Washroom />
   </div>
 );
