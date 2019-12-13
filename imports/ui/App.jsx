7dc820704294f8043e06d883dac9b9db703ef22e
graziela silva <grazims26@gmail.com>
2019-12-12 20:15:50 -0800

    Added a nav bar, cleaned up imports

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
-import Washroom from './WashroomInfo.jsx';
-import ReviewForm from './ReviewForm'
+import React from "react";
+import Hello from "./Hello.jsx";
+import Info from "./Info.jsx";
+import Washroom from "./WashroomInfo";
+import ReviewForm from "./ReviewForm";
+import NavBar from "./NavBar";
 
 const App = () => (
   <div>
-    <ReviewForm />
+    <NavBar />
+    {/* <ReviewForm /> */}
     {/* <Washroom /> */}
   </div>
 );
--- imports/ui/washroomInfo.jsx
+++ imports/ui/washroomInfo.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import { withTracker } from 'meteor/react-meteor-data';
-import { Washrooms }  from '../api/washrooms';
+import React, { Component } from "react";
+import { withTracker } from "meteor/react-meteor-data";
+import { Washrooms } from "../api/washrooms";
 
 class Washroom extends Component {
   render() {
     console.log(this.props);
-    const washrooms = this.props.washrooms.map(
-      washroom => this.makeLink(washroom)
+    const washrooms = this.props.washrooms.map(washroom =>
+      this.makeLink(washroom)
     );
 
     return (
       <div>
         <h2>Washrooms!</h2>
-        <ul>{ washrooms }</ul>
+        <ul>{washrooms}</ul>
       </div>
     );
   }
@@ -20,7 +20,9 @@ class Washroom extends Component {
   makeLink(washroom) {
     return (
       <li key={washroom._id}>
-        <a href="" target="_blank">{washroom.name}</a>
+        <a href="" target="_blank">
+          {washroom.name}
+        </a>
       </li>
     );
   }
@@ -28,6 +30,6 @@ class Washroom extends Component {
 
 export default WashroomContainer = withTracker(() => {
   return {
-    washrooms: Washrooms.find().fetch(),
+    washrooms: Washrooms.find().fetch()
   };
 })(Washroom);
