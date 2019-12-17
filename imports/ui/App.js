2476113d2d772e233a2f670d341a927f2f5ee79c
graziela silva <grazims26@gmail.com>
2019-12-16 20:38:14 -0800

    add facebook login

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReviewForm from "./ReviewForm";
 import NavBar from "./NavBar";
 import ListView from "./ListView";
-import AccountsUIWrapper from "./AccountsUIWrapper.js";
 
 const App = () => (
   <div>
