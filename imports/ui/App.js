ce0739f6957b5b3393ab6321c73a0b1b00b57759
Hongsoon Kim <khsun6226@naver.com>
2019-12-16 18:12:04 -0800

    Update App.js

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReviewForm from "./ReviewForm";
 import NavBar from "./NavBar";
 import ListView from "./ListView";
+import DetailView from "./DetailView"
 import AccountsUIWrapper from "./AccountsUIWrapper.js";
 
 const App = () => (
   <div>
     <NavBar />
     <ListView />
-    {/* <ReviewForm /> */}
+    <DetailView />
+    <ReviewForm />
   </div>
 );
 
