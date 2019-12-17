6f19f1ba8d61981acdf520a4544106697155f0c3
Hongsoon Kim <hkim@Hongsoons-MacBook-Pro.local>
2019-12-16 20:05:15 -0800

    add onclick event that appends the name of the clicked washroom

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReviewForm from "./ReviewForm";
 import NavBar from "./NavBar";
 import ListView from "./ListView"
+import DetailView from "./DetailView"
 
 const App = () => (
   <div>
     <NavBar />
     <ListView />
+    <DetailView />
     <ReviewForm />
   </div>
 );
