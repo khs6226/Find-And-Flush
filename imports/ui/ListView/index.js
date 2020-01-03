ceb6494141b6329bfa1bd00746e6e18b4fcece45
vancityjun <vancityjun@github.com>
2020-01-02 18:49:05 -0800

    add style & changed layout

--- imports/ui/ListView/index.js
+++ imports/ui/ListView/index.js
@@ -2,16 +2,9 @@ import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
 import React, { Component } from "react";
 import { withTracker } from "meteor/react-meteor-data";
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import Grid from "@material-ui/core/Grid";
-import Rating from "@material-ui/lab/Rating";
+import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-
-
 
 import { Washrooms } from "../../api/washrooms";
 import WashroomContext from "../../api/WashroomContext";
@@ -23,33 +16,37 @@ import ReviewForm from "../ReviewForm";
 // };
 
 class ListView extends Component {
-
   static contextType = WashroomContext;
+  componentDidMount = () => {
+    // console.log(this.context)
+  };
   renderWashrooms() {
-    return this.context.map(washroom => (
-      <WashroomItemList key={washroom._id} washroom={washroom} />
+    return this.context.map((washroom, i) => (
+      <WashroomItemList key={i} washroom={washroom} />
     ));
   }
 
   render() {
-     let { classes } = this.props;
-        console.log('props', this.props.washrooms)
-
-        const filter = () => {
-            console.log("filter")
-        }
+    let { classes } = this.props;
+    console.log("props", this.props.washrooms);
+    const filter = () => {
+      console.log("filter");
+    };
     return (
       <div className="listViewContainer">
         <header>
-          <h1>Washroom List</h1>
+          <Typography variant="h4" component="h1">
+            Washroom List
+          </Typography>
         </header>
-      <ReviewForm filter={filter} />
+        {/* <ReviewForm filter={filter} /> */}
 
-        <ul>{this.renderWashrooms()}</ul>
+        <Grid container spacing={2}>
+          {this.renderWashrooms()}
+        </Grid>
       </div>
     );
   }
 }
 
 export default ListView;
-
