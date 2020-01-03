c58164241847e9df4f8d7fb5ae8cef4b1e747b63
graziela silva <grazims26@gmail.com>
2020-01-02 20:29:13 -0800

    add position to json file and deleted from index

--- imports/ui/Maps/index.js
+++ imports/ui/Maps/index.js
@@ -9,18 +9,36 @@ const mapStyle = {
 };
 
 class Maps extends React.Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
   state = {
     activeMarker: {},
     selectedPlace: {},
     showingInfoWindow: false
   };
 
-  onMarkerClick = (props, marker) =>
+  onMarkerClick = (props, marker) => {
+    console.log(props, marker);
+    // this.setState({
+    //   activeMarker: marker,
+    //   selectedPlace: props,
+    //   showingInfoWindow: true
+    // });
+
+    // let propsCopy = JSON.parse(JSON.stringify(props));
+    let propsCopy = props;
+    propsCopy.position = { lat: 49.2828432, lng: -123.1190605 };
+
     this.setState({
       activeMarker: marker,
-      selectedPlace: props,
+      selectedPlace: propsCopy,
       showingInfoWindow: true
     });
+  };
+
   onInfoWindowClose = () =>
     this.setState({
       activeMarker: null,
@@ -35,6 +53,10 @@ class Maps extends React.Component {
       });
   };
 
+  componentDidMount = () => {
+    console.log(`my ref is ${this.myRef}`);
+  };
+
   render() {
     return (
       <Map
@@ -46,81 +68,12 @@ class Maps extends React.Component {
           lng: -123.1173159
         }}
       >
-        <Marker
+        {/* <Marker
           name="Public Washrooms"
           onClick={this.onMarkerClick}
           position={{ lat: 49.277912, lng: -123.1173159 }}
-        />
-        <Marker
-          name="David Lam Park"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.2770384, lng: -123.1082255 }}
-        />
-        <Marker
-          name="Harbour Green Park"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.289335, lng: -123.1217715 }}
-        />
-        <Marker
-          name="Canada Place"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.2883436, lng: -123.1153855 }}
-        />
-        <Marker
-          name="Granville Park"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.2588092, lng: -123.1420738 }}
-        />
-        <Marker
-          name="Tatlow Park"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.2695388, lng: -123.1692126 }}
-        />
-        <Marker
-          name="Hadden Park"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.2275369, lng: -123.1476764 }}
-        />
-        <Marker
-          name="Firehall Library"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.262872, lng: -123.137576 }}
-        />
-        <Marker
-          name="Grandview Park"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.2739592, lng: -123.0695801 }}
-        />
-        <Marker
-          name="Andy Livingstone Park"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.2788226, lng: -123.1057978 }}
-        />
-        <Marker
-          name="Stanley Park"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.2933866, lng: -123.139224 }}
-        />
-        <Marker
-          name="Carnegie Centre"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.281052, lng: -123.1001272 }}
-        />
-        <Marker
-          name="Richards Street"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.2759501, lng: -123.1238664 }}
-        />
-        <Marker
-          name="Public Washroom W Georgia"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.2828432, lng: -123.1190605 }}
-        />
-        <Marker
-          name="Main at Terminal Public Washroom"
-          onClick={this.onMarkerClick}
-          position={{ lat: 49.2730027, lng: -123.0994833 }}
-        />
+          ref={this.myRef}
+        /> */}
 
         <InfoWindow
           marker={this.state.activeMarker}
